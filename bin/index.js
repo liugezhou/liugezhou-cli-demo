13211855eec2ed61de6ffda250cab32558fb6f36
liumingzhou <18231133236@163.com>
2021-01-25 17:01:11 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -96,4 +96,19 @@ yargs()
       console.log(require.resolve.paths('dedent'))
     }
   })
+  .command({
+    command:'module',
+    aliases:["md"],
+    describe:'leran about module',
+    builder:(yargs)=>{},
+    handler:(argv)=>{
+      console.log('module.id: ', module.id);
+      console.log('module.exports: ', module.exports);
+      console.log('module.parent: ', module.parent);
+      console.log('module.filename: ', module.filename);
+      console.log('module.loaded: ', module.loaded);
+      console.log('module.children: ', module.children);
+      console.log('module.paths: ', module.paths);
+    }
+  })
   .parse(argv, context);
