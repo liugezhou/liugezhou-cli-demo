11ba0a9886f5e577391f7e78a632b14a925960e8
liumingzhou <18231133236@163.com>
2021-02-21 17:59:52 +0800

    exec callback

--- bin/process/index.js
+++ bin/process/index.js
@@ -1,8 +1,26 @@
 const cp = require('child_process')
 const path = require('path')
 
-cp.exec('ls -al|grep node_modules ',function(err,stdout,stderr){
-    console.log(err)
+const child = cp.exec('ls -al|grep node_modules',function(err,stdout,stderr){
+    console.log('callback--------start')
     console.log(stdout)
-    console.log(stderr)
+    console.log('callback--------end')
+})
+
+child.stdout.on('data',(chunk)=>{
+    console.log('stdout data:',chunk)
+})
+
+child.stderr.on('data',(chunk)=>{
+    console.log('stderr data:',chunk)
+})
+
+child.stderr.on('close',()=>{
+    console.log('stderr close')
+})
+child.on('exit',(exitCode)=>{
+    console.log('exit:',exitCode)
+})
+child.on('close',()=>{
+    console.log('close!')
 })
--- readme.md
+++ readme.md
@@ -18,3 +18,5 @@
 > bin/process/sync.js文件主要用来学习  node中child_process的同步方法execSync和execFileSync和spawnSync的使用。
 
 > bin/process/inherit.js文件主要用来学习  node中child_process的异步方法spawn的stdio属性使用。
+
+> bin/process/index.js文件主要用来学习  node中child_process的exec方法的源码以及回调监听的学习。
