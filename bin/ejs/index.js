04b58f8b67e8191ac9d33ceb3a07bcfeec4c0a88
liumingzhou <18231133236@163.com>
2021-03-05 16:27:20 +0800

    ejs源码解析

--- bin/ejs/index.js
+++ bin/ejs/index.js
@@ -3,7 +3,9 @@ const path = require('path')
 const fs = require('fs')
 // 第一种用法
 const html ='<div><%= user.name%></div>'
-const options = {}
+const options = {
+    debug:true
+}
 const data = {
     user:{
         name:'liugezhou',
@@ -13,23 +15,25 @@ const data = {
 }
 
 // 返回一个f于解析html模板中的function
-// const compileTemplate = ejs.compile(html,options)(data)
+// const template = ejs.compile(html,options)
+
+// const compileTemplate = template(data)
 
 // console.log(compileTemplate)
 
 //第二种用法
 // const renderTemplate = ejs.render(html,data,options)
 // console.log(renderTemplate)
-// 
-//第三种用法
-// const renderFile = ejs.renderFile(path.resolve(__dirname,'template.html'),data,options)
-// renderFile.then(res => console.log(res))
 
-ejs.fileLoader = function(filepath){
-    const content = fs.readFileSync(filepath).toString()
-    return '<div>from<%= user.copyright %></div>' + content
-}
-ejs.renderFile(path.resolve(__dirname,'template.html'),data,{delimiter:'%'},(err,res)=>{
-    console.log(res)
-})
+//第三种用法
+const renderFile = ejs.renderFile(path.resolve(__dirname,'template.html'),data,options)
+renderFile.then(res => console.log(res))
+
+// ejs.fileLoader = function(filepath){
+//     const content = fs.readFileSync(filepath).toString()
+//     return '<div>from<%= user.copyright %></div>' + content
+// }
+// ejs.renderFile(path.resolve(__dirname,'template.html'),data,{delimiter:'%'},(err,res)=>{
+//     console.log(res)
+// })
 
--- bin/index.js
+++ bin/index.js
@@ -2,4 +2,4 @@
 
 // require('../dist/core.js')
 
-require('./ejs/glob.js')
+require('./ejs')
